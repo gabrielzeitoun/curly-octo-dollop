f0565c7178847895b9d798c1da1ced5d9aae9b3a
z5414021 <z514021@ad.unsw.edu.au>
2024-05-20 02:43:20 +1000

    created toss() method for coin tosser

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function scrolldiv(name) {
 function scrolltop() {
     window.scrollTo(0, 0);
 }
+
+function toss() {
+    const rand = Math.random() < 0.5
+    const output_destination = document.getElementById("coin-tosser-result");
+    if (rand) {
+        output_destination.innerText = 'heads';
+    } else {
+        output_destination.innerText = 'tails';
+    }
+}
